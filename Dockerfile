e59208338c36c34d9f712719c8883a61d65a9d75
wakame-tech <kamata1919@gmail.com>
2024-04-30 22:37:05 +0900

    fix: ClusterConfiguration use kubectl to get pod ip addresses

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,10 @@ FROM wakametech/asdf-erlang-elixir:latest
 USER root
 RUN apt-get update && apt-get -y install libexpat1-dev inotify-tools iputils-ping net-tools
 
+RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl && \
+    chmod +x ./kubectl && \
+    mv ./kubectl /usr/local/bin/kubectl
+
 USER asdf
 WORKDIR /app
 
--- lib/cluster_configuration.ex
+++ lib/cluster_configuration.ex
@@ -4,15 +4,33 @@ alias Croma.Result, as: R
 defmodule AntikytheraK8s.ClusterConfiguration do
   @behaviour AntikytheraEal.ClusterConfiguration.Behaviour
 
+  defun get_pod_ip_and_statuses(label_app :: String.t()) :: Map.t(String.t(), boolean()) do
+    {res, 0} =
+      System.cmd("kubectl", [
+        "get",
+        "pods",
+        "-o",
+        "json"
+      ])
+
+    Jason.decode!(res)
+    |> Map.get("items")
+    |> Enum.filter(fn item ->
+      item["kind"] == "Pod" && item["metadata"]["labels"]["app"] == label_app
+    end)
+    |> Enum.map(fn item -> {item["status"]["podIP"], item["status"]["phase"] == "Running"} end)
+    |> Map.new()
+  end
+
   @impl true
   defun running_hosts() :: R.t(%{String.t() => boolean}) do
-    hosts = [
-      "antikythera-k8s-1.local",
-      "antikythera-k8s-2.local",
-      "antikythera-k8s-3.local"
-    ]
+    hosts_with_status = get_pod_ip_and_statuses("antikythera-k8s")
 
-    hosts_with_status = Enum.map(hosts, fn host -> {host, true} end) |> Map.new()
+    # hosts_with_status = %{
+    #   "antikythera-k8s-1.local" => true,
+    #   "antikythera-k8s-2.local" => true,
+    #   "antikythera-k8s-3.local" => true
+    # }
 
     {:ok, hosts_with_status}
   end
